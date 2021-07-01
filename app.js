1260745fb850beaeb13f902f6d419ab971b9f85a
Alyssa <aebowden36@gmail.com>
2021-06-30 23:27:45 -0400

    began first chart

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-d3.json("samples.json")
+d3.json("samples.json", function(samples){
+    console.log(samples)
+})
+
+var trace1 = {
+    x: samples.otu_ids,
+    y: samples.sample_values,
+    type: "bar"
+};
+
+var data = [trace1];
+
+var layout = {
+    title: "Top 10 OTUs"
+}
+
+Plotly.newPlot("plot", data, layout)
